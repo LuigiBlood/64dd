3b639ebb07f79c113c915fffc499aede1248fb85
LuigiBlood <luigiblood@gmail.com>
2019-07-31 23:57:39 +0200

    Add bytetolba

--- 64ddutils/bytetolba.c
+++ 64ddutils/bytetolba.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include "dd_globals.h"
 
-int lbatobyte(int disktype, int nlbas, int startlba)
+int lbatobyte(int disktype, int nbytes, int startlba)
 {
-    int totalbytes = 0;
     char init_flag = 1;
     int vzone = 1;
     int pzone = 0;
     int lba = startlba;
-    int lba_count = nlbas;
+    int lba_count = 0;
+    int byte_count = nbytes;
     int blkbytes = 0;
-    if (nlbas != 0)
+    if (nbytes != 0)
     {
-        for (; lba_count != 0; lba_count--)
+        do
         {
             if ((init_flag) || (VZONE_LBA_TBL[disktype][vzone] == lba))
             {
@@ -24,37 +24,45 @@ int lbatobyte(int disktype, int nlbas, int startlba)
                 }
                 blkbytes = BLOCK_SIZES[pzone];
             }
-            totalbytes += blkbytes;
+            if (byte_count < blkbytes)
+            {
+                byte_count = 0;
+            }
+            else
+            {
+                byte_count -= blkbytes;
+            }
             lba++;
+            lba_count++;
             init_flag = 0;
-            if ((lba_count != 0) && (lba > 0x10db))
+            if ((byte_count != 0) && (lba > 0x10db))
             {
                 return -1;
             }
-        }
+        } while (byte_count != 0);
     }
-    return totalbytes;
+    return lba_count;
 }
 
 int main(int argc, char const *argv[])
 {
     int disktype = 0;
-    int nlbas = 0;
+    int nbytes = 0;
     int startlba = 0;
     if (argc < 4)
     {
-        printf("usage: lbatobyte <type> <nlbas> <startlba>\n");
+        printf("usage: bytetolba <type> <nbytes> <startlba>\n");
     }
     else
     {
         if (sscanf(argv[1], "%d", &disktype) <= 0)
             return 1;
-        if (sscanf(argv[2], "%d", &nlbas) <= 0)
+        if (sscanf(argv[2], "%d", &nbytes) <= 0)
             return 1;
         if (sscanf(argv[3], "%d", &startlba) <= 0)
             return 1;
         
-        printf("%d\n", lbatobyte(disktype, nlbas, startlba));
+        printf("%d\n", lbatobyte(disktype, nbytes, startlba));
     }
     return 0;
 }
--- 64ddutils/lbatophys.c
+++ 64ddutils/lbatophys.c
@@ -6,52 +6,68 @@ int lbatophys(int lba, unsigned char* sys_data)
     int start_block = 0;
     int vzone, pzone = 0;
     int param_head, param_zone, param_cylinder = 0;
-    int vzone_lba, cylinder_temp, pzone_defect_num, defect_num = 0;
+    int vzone_lba, cylinder_zone_start, pzone_defect_offset, defect_amount = 0;
     
     int disktype = sys_data[5] & 0xF;
-    lba += 24;
 
-    //Unused here
+    //Skip System Area to correspond LBAs to SDK manual
+    //lba += 24;
+
+    //Unused here, get Block 0/1 on Disk Track
     if (((lba & 3) == 0) || ((lba & 3) == 3))
         start_block = 0;
     else
         start_block = 1;
     
+    //Get Virtual & Physical Disk Zones
     vzone = LBAToVZone(lba, disktype);
     pzone = VZoneToPZone(vzone, disktype);
 
+    //Get Disk Head
     param_head = (7 < pzone);
+
+    //Get Disk Zone
     param_zone = pzone;
     if (param_head != 0)
         param_zone = pzone - 7;
     
+    //Get Virtual Zone LBA start, if Zone 0, it's LBA 0
     if (vzone == 0)
         vzone_lba = 0;
     else
         vzone_lba = VZONE_LBA_TBL[disktype][vzone - 1];
     
-    param_cylinder = (lba - vzone_lba >> 1);
-    cylinder_temp = SCYL_ZONE_TBL[0][pzone];
+    //Calculate Physical Cylinder
+    param_cylinder = (lba - vzone_lba) >> 1;
+
+    //Get the start cylinder from current zone
+    cylinder_zone_start = SCYL_ZONE_TBL[0][pzone];
     if (param_head != 0)
     {
+        //If Head 1, count from the other way around
         param_cylinder = -param_cylinder;
-        cylinder_temp = OUTERCYL_TBL[param_zone - 15];
+        cylinder_zone_start = OUTERCYL_TBL[param_zone - 1];
     }
     param_cylinder += SCYL_ZONE_TBL[0][pzone];
 
+    //Get the relative offset to defect tracks for the current zone (if Zone 0, then it's 0)
     if (pzone == 0)
-        pzone_defect_num = 0;
+        pzone_defect_offset = 0;
     else
-        pzone_defect_num = sys_data[7 + pzone];
+        pzone_defect_offset = sys_data[8 + pzone - 1];
     
-    defect_num = sys_data[8 + pzone] - pzone_defect_num;
-    while ((defect_num != 0) && ((sys_data[0x20 + pzone_defect_num] + cylinder_temp) <= param_cylinder))
+    //Get amount of defect tracks for the current zone
+    defect_amount = sys_data[8 + pzone] - pzone_defect_offset;
+
+    //Skip defect tracks
+    while ((defect_amount != 0) && ((sys_data[0x20 + pzone_defect_offset] + cylinder_zone_start) <= param_cylinder))
     {
         param_cylinder++;
-        pzone_defect_num++;
-        defect_num--;
+        pzone_defect_offset++;
+        defect_amount--;
     }
 
+    //Return Cylinder and Head data for 64DD Seek command
     return param_cylinder | (param_head * 0x1000);
 }
 
